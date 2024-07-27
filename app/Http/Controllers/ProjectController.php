e7ea570ed93e17b23e8ace2ecc67f08e08f82264
imtyaz-17 <imtyazit17017@gmail.com>
2024-07-27 13:00:03 +0600

    ProjectController - functionality implemented

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProjectCreateRequest;
+use App\Http\Requests\ProjectUpdateRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        return ProjectResource::collection($request->user()->projects);
+    }
+
+    public function store(ProjectCreateRequest $request)
+    {
+        $data = $request->validated();
+        $project = $request->user()->projects()->create($data);
+
+        return new ProjectResource($project);
+    }
+    public function show(Project $project)
+    {
+        $project->load(['boards','boards.tickets']);
+        return new ProjectResource($project);
+    }
+    public function update(ProjectUpdateRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        abort_if($project->user_id !== $request->user()->id, 403,'You are not allowed to update this project');
+        $project->update($data);
+        return new ProjectResource($project);
+    }
+    public function destroy(Request $request, Project $project)
+    {
+        abort_if($project->user_id !== $request->user()->id, 403, "You are not allowed to delete this project");
+
+        $project->delete();
+
+        return response()->json([
+            'message' => 'Project deleted successfully'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -18,4 +21,24 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/project', [ProjectController::class, 'store']);
+    Route::get('/project/{project}', [ProjectController::class, 'show']);
+    Route::put('/project/{project}', [ProjectController::class, 'update']);
+    Route::put('/project/{project}', [ProjectController::class, 'update']);
+    Route::delete('/project/{project}', [ProjectController::class, 'destroy']);
+
+    Route::get('/project/{project}/boards', [BoardController::class, 'index']);
+    Route::post('/board', [BoardController::class, 'store']);
+    Route::get('/board/{board}', [BoardController::class, 'show']);
+    Route::put('/board/{board}', [BoardController::class, 'update']);
+    Route::delete('/board/{board}', [BoardController::class, 'destroy']);
+
+    Route::get('/tickets', [TicketController::class, 'index']);
+    Route::post('/ticket', [TicketController::class, 'store']);
+    Route::get('/ticket/{ticket}', [TicketController::class, 'show']);
+    Route::put('/ticket/{ticket}', [TicketController::class, 'update']);
+    Route::delete('/ticket/{ticket}', [TicketController::class, 'destroy']);
 });
